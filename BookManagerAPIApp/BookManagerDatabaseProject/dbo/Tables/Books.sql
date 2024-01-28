a2ecff2d2958e7471257efd51931a2d9451685c9
Pramod Shivaprasad <pramod2392@gmail.com>
2024-01-28 13:01:20 +0530

    Created a database project to keep track of the DB schema changes and the script to create the DB tables

---
+++ BookManagerAPIApp/BookManagerDatabaseProject/dbo/Tables/Books.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Books] (
+    [Id]            INT            IDENTITY (1, 1) NOT NULL,
+    [Name]          NVARCHAR (50)  NOT NULL,
+    [PurchasedDate] DATE           NULL,
+    [Price]         REAL           NULL,
+    [ImageBlobURL]  NVARCHAR (MAX) NULL,
+    [CategoryId]    INT            NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Books_Category] FOREIGN KEY ([CategoryId]) REFERENCES [dbo].[Category] ([Id])
+);
+
