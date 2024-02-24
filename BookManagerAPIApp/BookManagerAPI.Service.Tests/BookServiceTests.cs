d4978a865d25470851079d5eaa34c824b5126c9b
Pramod Shivaprasad <pramod2392@gmail.com>
2024-02-24 13:34:52 +0530

    Added unit test for service layer project.

---
+++ BookManagerAPIApp/BookManagerAPI.Service.Tests/BookServiceTests.cs
@@ -0,0 +1,51 @@
+﻿using BookManagerAPI.Repository.Interfaces;
+using BookManagerAPI.Repository.Models;
+using BookManagerAPI.Repository.Models.ResponseModels;
+using BookManagerAPI.Service.Impl;
+using BookManagerAPI.Service.Interfaces;
+using BookManagerAPI.Service.Models;
+using Castle.Core.Logging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookManagerAPI.Service.Tests
+{
+    [TestFixture]
+    public class BookServiceTests
+    {
+        [Test]
+        public async Task SaveImageToBlobAndAddNewBook_When_Called_Adds_New_Book()
+        {
+            //Arrange
+            Mock<ISQLDBRepository> _mockSQLDBRepository = new Mock<ISQLDBRepository>();
+            Mock<IAzureBlobRepository> _mockAzureBlobRepository = new Mock<IAzureBlobRepository>();
+            Mock<ILogger<BookService>> _mockBookServiceLogger = new Mock<ILogger<BookService>>();
+
+            UploadImageToBlobAsyncResponseModel uploadImageToBlobAsyncResponseModel = new(true,string.Empty, "sampleBlob");
+            AddBookModel addBookModel = new AddBookModel() { Name = "Sample", Price = 7.25M, CategoryId = 1, ImageBlobURL = "sampleURL", PurchasedDate = It.IsAny<DateTime>() };
+
+            _mockAzureBlobRepository.Setup(x => x.UploadImageToBlobAsync(It.IsAny<IFormFile>())).Returns(async () => uploadImageToBlobAsyncResponseModel);
+
+            _mockSQLDBRepository.Setup(x => x.AddNewBook(addBookModel)).Returns(async () => true);
+
+            IFormFile formFile = new FormFile(new MemoryStream(), 0, 11, "", "");
+
+            BookModel bookModel = new() { Name = "Sample", Price = 7.25M, CategoryId = 1, imageData = new byte[10], PurchasedDate = It.IsAny<DateTime>() };
+
+            // Act
+            IBookService bookService = new BookService(_mockSQLDBRepository.Object,_mockAzureBlobRepository.Object,_mockBookServiceLogger.Object);
+            var response = await bookService.SaveImageToBlobAndAddNewBook(formFile, bookModel);
+
+            //Assert
+            Assert.That(response.IsSuccess, Is.True);
+        }
+    }
+}
